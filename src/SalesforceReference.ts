3361625b8091ff121f328e2e929bbcfe5b124bd7
James Hill <me@jameshill.io>
2020-06-07 02:35:45 +1200

    Give doc retrieval approaches a heft DRYing

--- src/SalesforceReference.ts
+++ src/SalesforceReference.ts
@@ -1,4 +1,9 @@
 import * as vscode from 'vscode';
+import got from 'got/dist/source';
+
+const SF_DOC_ROOT_URL = 'https://developer.salesforce.com/docs';
+const SF_TOC_PATH = '/get_document';
+const SF_RAW_DOC_PATH = '/get_document_content';
 
 /**
  * A Salesforce Reference Entry represents a Salesforce ToC entry that:
@@ -39,15 +44,9 @@ export class SalesforceReferenceItem implements vscode.QuickPickItem {
     constructor(docNode: SalesforceTOC.DocumentationNode, breadcrumb: string) {
         this.label = docNode.text;
         if (!docNode.hasOwnProperty('a_attr') || docNode.a_attr === undefined) {
-            // console.log('SF rec docNode', docNode);
-            // console.log('ownProp?', docNode.hasOwnProperty('a_attr'));
-            // console.log('undefin?', docNode.a_attr === undefined);
             throw new Error('Supplied DocumentationNode had a missing or undefined `a_attr`. This CANNOT be used to build a SalesforceReferenceItem - these are used to build links and must have this populated');
         }
         if (!docNode.a_attr.hasOwnProperty('href') || docNode.a_attr.href === undefined) {
-            // console.log('SF rec docNode', docNode);
-            // console.log('ownProp?', docNode.a_attr.hasOwnProperty('href'));
-            // console.log('undefin?', docNode.a_attr.href === undefined);
             throw new Error('Supplied DocumentationNode had a missing or undefined `a_attr.href`. This CANNOT be used to build a SalesforceReferenceItem - these are used to build links and must have this populated');
         }
         this.href = docNode.a_attr.href;
@@ -72,6 +71,183 @@ export function convertDocNodeToSalesforceReferenceItem(referenceNode: any, brea
     return referenceItems;
 }
 
-export interface SalesforceReferenceType {
+abstract class SalesforceReferenceDocType {
 
+    /**
+     * The portion of the URL path giving the location of human readable doc for this doc type.
+     *
+     * e.g. in the URL "https://developer.salesforce.com/docs/atlas.en-us.apexcode.meta/apexcode/apex_dml_section.htm#apex_dml_undelete"
+     *      this is "/atlas.en-us.apexcode.meta/apexcode"
+     */
+    private readonly humanReadableDocPath: string;
+
+    /**
+     * The portion of the URL path giving the location of the Table of Contents used by tools to get document structure
+     *
+     * e.g. in the URL "https://developer.salesforce.com/docs/get_document/atlas.en-us.apexcode.meta"
+     *      this is "/atlas.en-us.apexcode.meta"
+     */
+    private readonly docTocPath: string;
+
+    /**
+     * The full URL to get the Table of Contents used by tools to get document structure
+     */
+    private readonly docTOCUrl: string;
+
+    constructor(docTocPath: string, humanReadableDocPath: string) {
+        this.docTocPath = docTocPath;
+        this.humanReadableDocPath = humanReadableDocPath;
+        this.docTOCUrl = SF_DOC_ROOT_URL + SF_TOC_PATH + this.docTocPath;
+    }
+
+    public async getSalesforceReferenceItems(): Promise<SalesforceReferenceItem[]> {
+        //TODO: handle any errors
+        const rootDocumentationNode: any = await this.getRootDocumentationNode();
+        return this.convertDocNodeToSalesforceReferenceItems(rootDocumentationNode, '$(home)');
+    }
+
+    /**
+     * Get a URL for a human-readable page that can be loaded into the browser, for a given SalesforceReferenceItem
+     * @param selectedReferenceItem A SalesforceReferenceItem to be loaded in the browser
+     */
+    public humanDocURL(selectedReferenceItem: SalesforceReferenceItem): string {
+        return `${SF_DOC_ROOT_URL}${this.humanReadableDocPath}/${selectedReferenceItem.href}`;
+    }
+
+    /**
+     * A recursive method that converts a SalesforceTOC.DocumentationNode into 0 to N SalesforceReferenceItem instances,
+     * by converting the node itself (if possible), and converting all of its children (if possible), putting them
+     * into an array and returning that
+     *
+     * @param documentationNode The node from the ToC to convert to Salesforce Reference Items
+     * @param breadcrumbString A human-readable breadcrumb string, indicating to the user where in the ToC this node sits (e.g. "$(home) $(breadcrumb-separator) Apex Language Reference")
+     */
+    private convertDocNodeToSalesforceReferenceItems(documentationNode: SalesforceTOC.DocumentationNode, breadcrumbString: string): SalesforceReferenceItem[] {
+        const referenceItems: SalesforceReferenceItem[] = [];
+        //Convert this node into a SalesforceReferenceItem, after run-time checking it has appropriate properties
+        if (documentationNode.hasOwnProperty('a_attr')) {
+            referenceItems.push(new SalesforceReferenceItem(documentationNode, breadcrumbString));
+        }
+        //Recursively convert children into SalesforceReferenceItems and add them to our list
+        if (documentationNode.hasOwnProperty('children') && documentationNode.children !== undefined) {
+            const breadcrumbStringForChildren = `${breadcrumbString} $(breadcrumb-separator) ${documentationNode.text}`;
+            documentationNode.children.forEach((childDocumentationNode: SalesforceTOC.DocumentationNode) => {
+                referenceItems.push(...this.convertDocNodeToSalesforceReferenceItems(childDocumentationNode, breadcrumbStringForChildren));
+            });
+        }
+        return referenceItems;
+    }
+
+    /**
+     * Get the root documentation node for this Documentation Type - its children
+     * will be the actual documentation we want to obtain
+     *
+     * TODO: In the event that we decide to expand what we pull from Docs in future,
+     *       this may need to account for multiple "root" nodes
+     */
+    protected abstract async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode>;
+
+    /**
+     * Get the Table of Contents used by tools to get document structure for this documentation type
+     */
+    protected async getDocTOC(): Promise<any> {
+        //TODO: Similar to the SalesforceTOC.DocumentationNode - the root node should be loaded into something with a defined interface
+        const body: any = await got(this.docTOCUrl).json();
+        return body;
+    }
+
+    /**
+     * UNIMPLEMENTED: Get a URL for a machine-readable page containing doc, given the supplied parameters
+     */
+    private rawDocURL(folder: string, id: string, locale: string, version: string): string {
+        // The required params can be obtained from the following things on the root Node.
+        //  See the NOTES.md file for further detail on possible future plans
+
+        // const vfFolder: string = sfJSONDoc.deliverable;
+        // const vfLocale: string = sfJSONDoc.language.locale;
+        // const vfDocVersion: string = sfJSONDoc.version.doc_version;
+
+        // return `${SF_DOC_ROOT_URL}${SF_RAW_DOC_PATH}/${folder}/${id}/${locale}/${version}`;
+        throw new Error('This method is not yet implemented');
+    }
 }
+
+class ApexSalesforceReferenceDocType extends SalesforceReferenceDocType {
+    constructor() {
+        super(
+            '/atlas.en-us.apexcode.meta',
+            '/atlas.en-us.apexcode.meta/apexcode'
+        );
+    }
+    protected async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode> {
+        //TODO: handle any errors
+        const apexDocToc: any = await this.getDocTOC();
+        return apexDocToc.toc[0].children.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'apex_reference');
+    }
+}
+
+class VisualforceSalesforceReferenceDocType extends SalesforceReferenceDocType {
+    constructor() {
+        super(
+            '/atlas.en-us.pages.meta',
+            '/atlas.en-us.pages.meta/pages'
+        );
+    }
+    protected async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode> {
+        //TODO: handle any errors
+        const vfDocToc: any = await this.getDocTOC();
+        return vfDocToc.toc.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'pages_compref');
+    }
+}
+
+class LightningConsoleSalesforceReferenceDocType extends SalesforceReferenceDocType {
+    constructor() {
+        super(
+            '/atlas.en-us.api_console.meta',
+            '/atlas.en-us.api_console.meta/api_console'
+        );
+    }
+    protected async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode> {
+        //TODO: handle any errors
+        const lightningConsoleDocToc: any = await this.getDocTOC();
+        const lightningconsoleTopLevelToc: any = lightningConsoleDocToc.toc.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_js_getting_started');
+        return lightningconsoleTopLevelToc.children.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_methods_lightning');
+    }
+}
+
+class ClassicConsoleSalesforceReferenceDocType extends SalesforceReferenceDocType {
+    constructor() {
+        super(
+            '/atlas.en-us.api_console.meta',
+            '/atlas.en-us.api_console.meta/api_console'
+        );
+    }
+    protected async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode> {
+        //TODO: handle any errors
+        const classicConsoleDocToc: any = await this.getDocTOC();
+        const classicconsoleTopLevelToc: any = classicConsoleDocToc.toc.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_intro');
+        return classicconsoleTopLevelToc.children.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_methods_classic');
+    }
+}
+
+class MetadataSalesforceReferenceDocType extends SalesforceReferenceDocType {
+    constructor() {
+        super(
+            '/atlas.en-us.api_meta.meta',
+            '/atlas.en-us.api_meta.meta/api_meta'
+        );
+    }
+    protected async getRootDocumentationNode(): Promise<SalesforceTOC.DocumentationNode> {
+        //TODO: handle any errors
+        const metadataDocToc: any = await this.getDocTOC();
+        return metadataDocToc.toc.find((node: SalesforceTOC.DocumentationNode) => node.hasOwnProperty('text') && node.text === 'Reference');
+    }
+}
+
+export const SalesforceReferenceDocTypes: any = {
+    APEX: new ApexSalesforceReferenceDocType(),
+    VISUALFORCE: new VisualforceSalesforceReferenceDocType(),
+    LIGHTNING_CONSOLE: new LightningConsoleSalesforceReferenceDocType(),
+    CLASSIC_CONSOLE: new ClassicConsoleSalesforceReferenceDocType(),
+    METADATA: new MetadataSalesforceReferenceDocType(),
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,183 +1,85 @@
 import * as vscode from 'vscode';
-import got from 'got';
 import './SalesforceReference';
-import { SalesforceReferenceItem, convertDocNodeToSalesforceReferenceItem } from './SalesforceReference';
+import { SalesforceReferenceItem, convertDocNodeToSalesforceReferenceItem, SalesforceReferenceDocTypes } from './SalesforceReference';
 
 export async function activate(context: vscode.ExtensionContext) {
 
-    const SF_DOC_ROOT_URL = 'https://developer.salesforce.com/docs';
-    const SF_TOC_PATH = '/get_document';
-    const SF_RAW_DOC_PATH = '/get_document_content';
-
-    const APEX_HUMAN_DOC_PATH = '/atlas.en-us.apexcode.meta/apexcode';
-    const APEX_DOC_TOC_URL = SF_DOC_ROOT_URL + SF_TOC_PATH + '/atlas.en-us.apexcode.meta';
-
-    const VF_HUMAN_DOC_PATH = '/atlas.en-us.pages.meta/pages';
-    const VF_DOC_TOC_URL = SF_DOC_ROOT_URL + SF_TOC_PATH + '/atlas.en-us.pages.meta';
-
-    const SFCONSOLE_HUMAN_DOC_PATH = '/atlas.en-us.api_console.meta/api_console';
-    const SFCONSOLE_DOC_TOC_URL = SF_DOC_ROOT_URL + SF_TOC_PATH + '/atlas.en-us.api_console.meta';
-
-    const METADATA_HUMAN_DOC_PATH = '/atlas.en-us.api_meta.meta/api_meta';
-    const METADATA_DOC_TOC_URL = SF_DOC_ROOT_URL + SF_TOC_PATH + '/atlas.en-us.api_meta.meta';
-
-
-    async function getApexDocToc(): Promise<any> {
-        const body: any = await got(APEX_DOC_TOC_URL).json();
-        return body;
-    }
-
-    async function getVisualforceDocToc(): Promise<any> {
-        const body: any = await got(VF_DOC_TOC_URL).json();
-        return body;
-    }
-
-    async function getServiceConsoleDocToc(): Promise<any> {
-        const body: any = await got(SFCONSOLE_DOC_TOC_URL).json();
-        return body;
-    }
-
-    async function getMetadataDocToc(): Promise<any> {
-        const body: any = await got(METADATA_DOC_TOC_URL).json();
-        return body;
-    }
-
-    function buildApexHumanDocURL(selectedReferenceItem: SalesforceReferenceItem) {
-        return `${SF_DOC_ROOT_URL}${APEX_HUMAN_DOC_PATH}/${selectedReferenceItem.href}`;
-    }
-
-    function buildVisualforceHumanDocURL(selectedReferenceItem: SalesforceReferenceItem) {
-        return `${SF_DOC_ROOT_URL}${VF_HUMAN_DOC_PATH}/${selectedReferenceItem.href}`;
-    }
-
-    function buildConsoleHumanDocURL(selectedReferenceItem: SalesforceReferenceItem) {
-        return `${SF_DOC_ROOT_URL}${SFCONSOLE_HUMAN_DOC_PATH}/${selectedReferenceItem.href}`;
-    }
-
-    function buildMetadataHumanDocURL(selectedReferenceItem: SalesforceReferenceItem) {
-        return `${SF_DOC_ROOT_URL}${METADATA_HUMAN_DOC_PATH}/${selectedReferenceItem.href}`;
-    }
-
-    function buildApexRawDocURL(folder: string, id: string, locale: string, version: string): string {
-        return `${SF_DOC_ROOT_URL}${SF_RAW_DOC_PATH}/${folder}/${id}/${locale}/${version}`;
-    }
-
     let apexReferenceDisposable: vscode.Disposable = vscode.commands.registerCommand('vscode-salesforce-doc-lookup.salesforce-reference-apex', async () => {
-        // var foo: SalesforceReferenceItem = new SalesforceReferenceItem({text: 'foo'},'bar');
-
-        //TODO: review icon usage in this command https://code.visualstudio.com/api/references/icons-in-labels
         //TODO: caching
 
         //todo: handle errs with try catch fin, maybe show something dynamic in the message, like the bounce in the old ST3 plugin
         vscode.window.showInformationMessage('Retrieving Salesforce Apex Reference Index...','OK');
-        const sfJSONDoc: any = await getApexDocToc();
-        const apexFolder: string = sfJSONDoc.deliverable;
-        const apexLocale: string = sfJSONDoc.language.locale;
-        const apexDocVersion: string = sfJSONDoc.version.doc_version;
-        const apexReferenceToc: SalesforceTOC.DocumentationNode = sfJSONDoc.toc[0].children.find((node: any) => node.hasOwnProperty('id') && node.id === 'apex_reference');
 
-        const apexReferenceTocQuickPickItems = convertDocNodeToSalesforceReferenceItem(apexReferenceToc ,'$(home)');
-
-        var myNeatItems: SalesforceReferenceItem[] = apexReferenceTocQuickPickItems;
+        var salesforceReferenceItems: SalesforceReferenceItem[] = SalesforceReferenceDocTypes.APEX.getSalesforceReferenceItems();
 
         //TODO handle errors
-        vscode.window.showQuickPick(myNeatItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
+        vscode.window.showQuickPick(salesforceReferenceItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
             if (selectedReferenceItem !== undefined) {
-                vscode.env.openExternal(vscode.Uri.parse(buildApexHumanDocURL(selectedReferenceItem!)));
+                vscode.env.openExternal(vscode.Uri.parse(SalesforceReferenceDocTypes.APEX.humanDocURL(selectedReferenceItem!)));
             }
         });
     });
 
     let vfReferenceDisposable: vscode.Disposable = vscode.commands.registerCommand('vscode-salesforce-doc-lookup.salesforce-reference-visualforce', async () => {
-        //TODO: review icon usage in this command https://code.visualstudio.com/api/references/icons-in-labels
         //TODO: caching
 
         //todo: handle errs with try catch fin, maybe show something dynamic in the message, like the bounce in the old ST3 plugin
         vscode.window.showInformationMessage('Retrieving Salesforce Visualforce Reference Index...','OK');
-        const sfJSONDoc: any = await getVisualforceDocToc();
-        const vfFolder: string = sfJSONDoc.deliverable;
-        const vfLocale: string = sfJSONDoc.language.locale;
-        const vfDocVersion: string = sfJSONDoc.version.doc_version;
-        const vfReferenceToc: SalesforceTOC.DocumentationNode = sfJSONDoc.toc.find((node: any) => node.hasOwnProperty('id') && node.id === 'pages_compref');
 
-        const vfReferenceTocQuickPickItems = convertDocNodeToSalesforceReferenceItem(vfReferenceToc ,'$(home)');
-
-        var myNeatItems: SalesforceReferenceItem[] = vfReferenceTocQuickPickItems;
+        var salesforceReferenceItems: SalesforceReferenceItem[] = SalesforceReferenceDocTypes.VISUALFORCE.getSalesforceReferenceItems();
 
         //TODO handle errors
-        vscode.window.showQuickPick(myNeatItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
+        vscode.window.showQuickPick(salesforceReferenceItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
             if (selectedReferenceItem !== undefined) {
-                vscode.env.openExternal(vscode.Uri.parse(buildVisualforceHumanDocURL(selectedReferenceItem!)));
+                vscode.env.openExternal(vscode.Uri.parse(SalesforceReferenceDocTypes.VISUALFORCE.humanDocURL(selectedReferenceItem!)));
             }
         });
     });
 
     let lightningconsoleReferenceDisposable: vscode.Disposable = vscode.commands.registerCommand('vscode-salesforce-doc-lookup.salesforce-reference-lightning-console', async () => {
-        //TODO: review icon usage in this command https://code.visualstudio.com/api/references/icons-in-labels
         //TODO: caching
 
         //todo: handle errs with try catch fin, maybe show something dynamic in the message, like the bounce in the old ST3 plugin
         vscode.window.showInformationMessage('Retrieving Salesforce Lightning Console Reference Index...','OK');
-        const sfJSONDoc: any = await getServiceConsoleDocToc();
-        const lightningconsoleFolder: string = sfJSONDoc.deliverable;
-        const lightningconsoleLocale: string = sfJSONDoc.language.locale;
-        const lightningconsoleDocVersion: string = sfJSONDoc.version.doc_version;
-        const lightningconsoleTopLevelToc: any = sfJSONDoc.toc.find((node: any) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_js_getting_started');
-        const lightningconsoleReferenceToc: SalesforceTOC.DocumentationNode = lightningconsoleTopLevelToc.children.find((node: any) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_methods_lightning');
 
-        const referenceTocQuickPickItems = convertDocNodeToSalesforceReferenceItem(lightningconsoleReferenceToc ,'$(home)');
+        var salesforceReferenceItems: SalesforceReferenceItem[] = SalesforceReferenceDocTypes.LIGHTNING_CONSOLE.getSalesforceReferenceItems();
 
         //TODO handle errors
-        vscode.window.showQuickPick(referenceTocQuickPickItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
+        vscode.window.showQuickPick(salesforceReferenceItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
             if (selectedReferenceItem !== undefined) {
-                vscode.env.openExternal(vscode.Uri.parse(buildConsoleHumanDocURL(selectedReferenceItem!)));
+                vscode.env.openExternal(vscode.Uri.parse(SalesforceReferenceDocTypes.LIGHTNING_CONSOLE.humanDocURL(selectedReferenceItem!)));
             }
         });
     });
 
     let classicconsoleReferenceDisposable: vscode.Disposable = vscode.commands.registerCommand('vscode-salesforce-doc-lookup.salesforce-reference-classic-console', async () => {
-        //TODO: review icon usage in this command https://code.visualstudio.com/api/references/icons-in-labels
         //TODO: caching
 
         //todo: handle errs with try catch fin, maybe show something dynamic in the message, like the bounce in the old ST3 plugin
         vscode.window.showInformationMessage('Retrieving Salesforce Classic Console Reference Index...','OK');
-        const sfJSONDoc: any = await getServiceConsoleDocToc();
-        const classicconsoleFolder: string = sfJSONDoc.deliverable;
-        const classicconsoleLocale: string = sfJSONDoc.language.locale;
-        const classicconsoleDocVersion: string = sfJSONDoc.version.doc_version;
-        const classicconsoleTopLevelToc: any = sfJSONDoc.toc.find((node: any) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_intro');
-        const classicconsoleReferenceToc: SalesforceTOC.DocumentationNode = classicconsoleTopLevelToc.children.find((node: any) => node.hasOwnProperty('id') && node.id === 'sforce_api_console_methods_classic');
 
-        const referenceTocQuickPickItems = convertDocNodeToSalesforceReferenceItem(classicconsoleReferenceToc ,'$(home)');
+        var salesforceReferenceItems: SalesforceReferenceItem[] = SalesforceReferenceDocTypes.CLASSIC_CONSOLE.getSalesforceReferenceItems();
 
         //TODO handle errors
-        vscode.window.showQuickPick(referenceTocQuickPickItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
+        vscode.window.showQuickPick(salesforceReferenceItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
             if (selectedReferenceItem !== undefined) {
-                vscode.env.openExternal(vscode.Uri.parse(buildConsoleHumanDocURL(selectedReferenceItem!)));
+                vscode.env.openExternal(vscode.Uri.parse(SalesforceReferenceDocTypes.CLASSIC_CONSOLE.humanDocURL(selectedReferenceItem!)));
             }
         });
     });
 
     let metadataReferenceDisposable: vscode.Disposable = vscode.commands.registerCommand('vscode-salesforce-doc-lookup.salesforce-reference-metadata', async () => {
-        //TODO: review icon usage in this command https://code.visualstudio.com/api/references/icons-in-labels
         //TODO: caching
 
         //todo: handle errs with try catch fin, maybe show something dynamic in the message, like the bounce in the old ST3 plugin
         vscode.window.showInformationMessage('Retrieving Salesforce Metadata Reference Index...','OK');
-        const sfJSONDoc: any = await getMetadataDocToc();
-        console.dir(sfJSONDoc);
-        const metadataFolder: string = sfJSONDoc.deliverable;
-        const metadataLocale: string = sfJSONDoc.language.locale;
-        const metadataDocVersion: string = sfJSONDoc.version.doc_version;
-        const metadataReferenceToc: SalesforceTOC.DocumentationNode = sfJSONDoc.toc.find((node: any) => node.hasOwnProperty('text') && node.text === 'Reference');
-        console.dir(metadataReferenceToc);
 
-        const referenceTocQuickPickItems = convertDocNodeToSalesforceReferenceItem(metadataReferenceToc,'$(home)');
+        var salesforceReferenceItems: SalesforceReferenceItem[] = SalesforceReferenceDocTypes.METADATA.getSalesforceReferenceItems();
 
         //TODO handle errors
-        vscode.window.showQuickPick(referenceTocQuickPickItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
+        vscode.window.showQuickPick(salesforceReferenceItems, {matchOnDetail: true}).then((selectedReferenceItem) => {
             if (selectedReferenceItem !== undefined) {
-                vscode.env.openExternal(vscode.Uri.parse(buildMetadataHumanDocURL(selectedReferenceItem!)));
+                vscode.env.openExternal(vscode.Uri.parse(SalesforceReferenceDocTypes.METADATA.humanDocURL(selectedReferenceItem!)));
             }
         });
     });
